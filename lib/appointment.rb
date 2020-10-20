a26bb0a682fc82a2e196dab302dfbebf6ed026ad
apontigo <PontigoAndres@gmail.com>
2020-10-20 01:37:06 -0400

    Done.

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,17 +1,13 @@
 class Appointment
-
-    attr_accessor :date, :patient, :doctor
-   @@all = []
-
-    def initialize(patient, date, doctor)
-     @patient = patient
-     @date = date
-     @doctor = doctor
-     @@all << self
-   end
-
-    def self.all
-     @@all
-   end
- end 
- 
+  attr_accessor :patient, :doctor, :date
+  @@all = []
+  def initialize(date, patient, doctor)
+      @date = date
+      @patient = patient
+      @doctor = doctor
+      @@all << self
+  end
+  def self.all
+      @@all
+  end
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,27 +1,24 @@
 class Patient
-
-    attr_accessor :name, :appointment, :doctor
-   @@all = []
-
-    def initialize(name)
-     @name = name
-     @@all << self
-   end
-
-    def self.all
-     @@all
-   end
-
-    def appointments
-     Appointment.all.select {|appointment| appointment.patient == self}
-   end
-
-    def doctors
-     appointments.collect {|appointment| appointment.doctor}
-   end
-
-    def new_appointment(doctor, date)
-     Appointment.new(self, date, doctor)
-   end
-
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+      @name = name
+      @@all << self
   end
+  def self.all
+      @@all
+  end
+  def appointments
+      Appointment.all.select do |appointment|
+          appointment.patient == self
+      end
+  end
+  def doctors
+      appointments.map do |appointment|
+          appointment.doctor
+      end
+  end
+  def new_appointment(doctor, date)
+      Appointment.new(date, self, doctor)
+  end
+end
